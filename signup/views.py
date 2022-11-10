3fae2745f26c883aa4f3ee2d5376788fc669cfc2
Anuj Saroj <srjanuj255@gmail.com>
2022-11-10 08:28:39 +0530

    initial commit

---
+++ signup/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+import mysql.connector as sql
+fn=''
+ln=''
+s=''
+em=''
+pwd=''
+# Create your views here.
+def signaction(request):
+    global fn,ln,s,em,pwd
+    if request.method=="POST":
+        m=sql.connect(host="localhost",user="root",passwd="Anuj@143",database='website')
+        cursor=m.cursor()
+        d=request.POST
+        for key,value in d.items():
+            if key=="first_name":
+                fn=value
+            if key=="last_name":
+                ln=value
+            if key=="sex":
+                s=value
+            if key=="email":
+                em=value
+            if key=="password":
+                pwd=value
+        
+        c="insert into users Values('{}','{}','{}','{}','{}')".format(fn,ln,s,em,pwd)
+        cursor.execute(c)
+        m.commit()
+
+    return render(request,'signup_page.html')
+
+def products(request):
+    return(request,'product.html')
+
+
+def products(request):
+    return(request,'cart.html')
+
+
+def products(request):
+    return(request,'product_details.html')
+
+
